a06b44faef65b76ff7406662aa419804aebf3458
lemontree356 <joyce356@foxmail.com>
2020-09-23 11:00:41 +0800

    login add img

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-      login...
+    <div class="login_container">
+      <div class="login_box">
+        <div class="avatar_box">
+          <img src="../assets/logo.png" />
+        </div>
+      </div>
     </div>
 </template>
 
@@ -10,6 +14,41 @@
     }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
 
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+
+    .avatar_box {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      box-shadow: 0 0 10px #ddd;
+      padding: 10px;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
 </style>
